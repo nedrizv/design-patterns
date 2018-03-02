feb6bd5fd602d876e24975c30797698718cbd989
Sayed Rizvi <sayednaweed.rizvi@lexisnexis.com>
2018-03-02 16:49:33 +0530

    Facotry pattern

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+// Factory design pattern
+
+abstract class VehicleFactory
+{
+    
+}
+
+class VehicleFactory
+{
+
+}
+
+abstract class MotorVehicleFactory
+{
+    
+}
+
+class Motor
+{
+    
+}
+
